d3506b85f25338ecc3362f9fcfc4121e487341dd
Anton Nikitin <antonnikitin@live.co.uk>
2026-03-14 18:52:18 +0000

    Dynamically generate STARTING IN and STARTING SOON

--- static/index.js
+++ static/index.js
@@ -9,9 +9,9 @@ function showMessage(text) {
 function updateCountdown(target) {
     const duration = target.diffNow();
     if (duration.valueOf() <= 0) {
-        showMessage("Soon!");
+        showMessage("Starting \nSoon!");
     } else {
-        document.getElementById(COUNTDOWN_ELEMENT_ID).innerText = `in ${duration.toFormat("hh:mm:ss")}`;
+        document.getElementById(COUNTDOWN_ELEMENT_ID).innerText = `Starting in \n${duration.toFormat("hh:mm:ss")}`;
     }
 }
 
